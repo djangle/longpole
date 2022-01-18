9669c46d33b48f005b169aa40c021785a39b354d
Adam Amodio <acamodio@gmail.com>
2022-01-18 11:25:47 -0800

    Clean-up

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,48 @@
+//pages/index.js
+
 import Head from "next/head";
 import clientPromise from "../lib/mongodb";
 
-export default function Index({ isConnected }) {
+export default function Users({ pageTitle, isConnected, users }) {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Longpole</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <h1>{pageTitle}</h1>
+        
+        <br></br>
+        <table className="table-auto text-left shadow-lg bg-white">
+          <thead>
+            <tr>
+              <th></th>
+              <th className="bg-blue-100 border text-left px-8 py-1">_id</th>
+              <th className="bg-blue-100 border text-left px-8 py-1">
+                Username
+              </th>
+              <th className="bg-blue-100 border text-left px-8 py-1">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key="{user._id}">
+                <td className="border px-2 py-1 hover:bg-red-600">#</td>
+                <td className="border px-8 py-1">{user._id}</td>
+                <td className="border px-8 py-1">{user.username}</td>
+                <td className="border px-8 py-1">{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
 
       <footer>
+        <br></br>
         {isConnected ? (
-          <h2>You are connected to MongoDB</h2>
+          <h2>You are connected to MongoDB.</h2>
         ) : (
           <h2>
             You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
@@ -21,7 +50,6 @@ export default function Index({ isConnected }) {
           </h2>
         )}
       </footer>
-
     </div>
   );
 }
@@ -33,10 +61,15 @@ export async function getServerSideProps(context) {
     // const db = client.db("myDatabase");
     // Then you can execute queries against your database like so:
     // db.find({}) or any of the MongoDB Node Driver commands
-    await clientPromise;
+    const client = await clientPromise;
+
+    const db = client.db("longpole");
+
+    let users = await db.collection("users").find({}).toArray();
+    users = JSON.parse(JSON.stringify(users));
+
     return {
-      props: { isConnected: true },
- 
+      props: { pageTitle: "longpole://pages/index.js", isConnected: true, users },
     };
   } catch (e) {
     console.error(e);
